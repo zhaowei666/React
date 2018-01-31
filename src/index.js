916fc72a6d9747ec40d731c7110013c608a48fd4
Zhaowei Ren <rzwdsg@gmail.com>
2018-01-30 23:52:25 -0500

    added multiple routers

--- src/index.js
+++ src/index.js
@@ -1,73 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom'
 import './index.css';
+import Game from './game';
+import createHistory from 'history/createBrowserHistory';
+const history = createHistory();
 
-class Square extends React.Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-          value: null,
-      };
-  }
-  render() {
-    return (
-      <button className="square" onClick={() => this.setState({'value': 'X'})}>
-        {this.state.value}
-      </button>
-    );
-  }
-}
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
-  }
-
-  render() {
-    const status = 'Next player: X';
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-class Game extends React.Component{
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
+class Hello extends React.Component {
+    render() {
+        return (
+            <h1>Hello World</h1>
+        );
+    }
 }
 
 // ========================================
 
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
+ReactDOM.render((
+  <BrowserRouter history={history}>
+    <div>
+      <Route exact path='/' component={Hello} />
+      <Route path='/hello' component={Hello} />
+      <Route path='/game' component={Game} />
+    </div>
+  </BrowserRouter>
+), document.getElementById('root'));
+
