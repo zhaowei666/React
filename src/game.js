d4fb916efe554dcd4d6eb5b6d25a33af8c3dbd18
Zhaowei Ren <rzwdsg@gmail.com>
2018-01-31 22:50:10 -0500

    added take-turns to game

--- src/game.js
+++ src/game.js
@@ -17,29 +17,44 @@ class Game extends React.Component{
   }
 }
 
-class Square extends React.Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-          value: null,
-      };
-  }
-  render() {
-    return (
-      <button className="square" onClick={() => this.setState({'value': 'X'})}>
-        {this.state.value}
-      </button>
-    );
-  }
+
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onclick}>
+      {props.value}
+    </button>
+  );
 }
 
+
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    };
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
+  }
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onclick={() => this.handleClick(i)}
+      />
+    )
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
