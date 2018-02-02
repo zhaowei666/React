54366355e83f4b09be2aadd2899cef4904b314f4
Zhaowei Ren <rzwdsg@gmail.com>
2018-02-01 22:03:45 -0500

    added winner declaration of game

--- src/game.js
+++ src/game.js
@@ -33,7 +33,8 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
-      xIsNext: true
+      xIsNext: true,
+      winner: null
     };
   }
   handleClick(i) {
@@ -41,7 +42,8 @@ class Board extends React.Component {
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
-      xIsNext: !this.state.xIsNext
+      xIsNext: !this.state.xIsNext,
+      winner: calculateWinner(squares)
     });
   }
   renderSquare(i) {
@@ -54,7 +56,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = this.state.winner;
+    let status;
+    if (winner) {
+      status = 'Winner is ' + winner;
+    } else {
+      status = 'Next player is ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -77,6 +85,28 @@ class Board extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [2, 4, 7],
+    [3, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[b] && squares[c]) {
+      if (squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+  }
+  return null
 }
 
 export default Game;
