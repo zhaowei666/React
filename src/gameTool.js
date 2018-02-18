3d53b28ca33bc1023821c487d677050a32f88965
Zhaowei Ren <rzwdsg@gmail.com>
2018-02-17 22:57:02 -0500

    added characterCheck of gameTool

--- src/gameTool.js
+++ src/gameTool.js
@@ -31,10 +31,7 @@ class RoomCreater extends React.Component{
     fetch((url), {
       method: "GET",
     })
-      .then(res => {
-        return res.json()
-        }
-      )
+      .then(res => res.json())
       .then(res => {
         this.setState({
           room: res.room
@@ -65,15 +62,10 @@ class RoomCreater extends React.Component{
     const room = this.state.room;
     let roomMessage;
     if (room) {
-      roomMessage = "You room has been created successfully. The room number is" + room + ".";
+      roomMessage = "You room has been created successfully. The room number is " + room + ".";
     }
-    const characters = JSON.stringify(this.state.characters);
     return (
       <div>
-        <p>haha</p>
-        <div>{room}</div>
-        <div>{characters}</div>
-        <p>hehe</p>
         <div><p>{roomMessage}</p></div>
         <form>
           {this.renderCharacterInput('Merlin')}
@@ -101,10 +93,84 @@ function CharacterInput(props) {
 
 
 class CharacterCheck extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      room: '',
+      card: '',
+      status: 0
+    };
+    this.roomOnChane = this.roomOnChane.bind(this);
+    this.nameOnChange = this.nameOnChange.bind(this);
+    this.submitCheck = this.submitCheck.bind(this);
+  }
+
+  nameOnChange({target}) {
+    this.setState({
+      name: target.value
+    });
+  }
+
+  roomOnChane({target}) {
+    this.setState({
+      room: target.value
+    });
+  }
+  submitCheck() {
+    const room = this.state.room;
+    const name = this.state.name;
+    const url = "http://127.0.0.1:8000/game_tool/draw_character?name=" + name + "&room=" + room;
+    fetch((url), {
+      method: "GET"
+    })
+      .then(res => {
+        this.setState({
+          status: res.status
+        });
+        return res.json();
+      })
+      .then(res => {
+        this.setState({
+          card: res.character,
+        });
+      })
+      .catch(error => {
+        alert(error);
+      });
+  }
+
   render() {
+    const card = this.state.card;
+    const status = this.state.status;
+    let cardMessage;
+    if (status === 202) {
+      cardMessage = "Sorry! No cards left in the pool";
+    } else if (status === 201) {
+      if (card) {
+        cardMessage = "You are " + card + " in this game! Good Luck";
+      }
+    } else if (status === 200) {
+      if (card) {
+        cardMessage = "You drew a card before. You are " + card + ".";
+      }
+    }
     return (
       <div>
-
+        <div>
+          <p>{cardMessage}</p>
+        </div>
+        <div>
+          <label>Nick Name
+            <input type="queryText" placeholder="Enter your nickname" onChange={(target) => this.nameOnChange(target)} />
+          </label>
+        </div>
+        <div>
+          <label>Room Number
+            <input type="queryText" placeholder="Enter room number" onChange={(target) => this.roomOnChane(target)} />
+          </label>
+        </div>
+        <input type="button" value="See your card" onClick={this.submitCheck} />
       </div>
     )
   }
